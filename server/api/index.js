4a57d9653c42fef023c035535fccdae4658d3cc5
Manasa-Rayi <manasa@tektreeinc.com>
2025-01-15 23:28:47 -0500

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,38 +1,72 @@
-const { Server } = require("socket.io");
 
-const io = new Server(8000, {
-  cors: true,
-});
+import { Server } from "socket.io";
+import { createServer } from "http";
 
-const emailToSocketIdMap = new Map();
-const socketidToEmailMap = new Map();
-
-io.on("connection", (socket) => {
-  console.log(`Socket Connected`, socket.id);
-  socket.on("room:join", (data) => {
-    const { email, room } = data;
-    emailToSocketIdMap.set(email, socket.id);
-    socketidToEmailMap.set(socket.id, email);
-    io.to(room).emit("user:joined", { email, id: socket.id });
-    socket.join(room);
-    io.to(socket.id).emit("room:join", data);
+export default (req, res) => {
+  // Create HTTP server and socket.io server
+  const server = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Socket.IO server running');
   });
 
-  socket.on("user:call", ({ to, offer }) => {
-    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  const io = new Server(server, {
+    cors: {
+      origin: "https://webrtc-123.vercel.app", // Specify your frontend URL here
+      methods: ["GET", "POST"],
+      allowedHeaders: ["Content-Type"],
+    },
   });
 
-  socket.on("call:accepted", ({ to, ans }) => {
-    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  // Maps to track connections
+  const emailToSocketIdMap = new Map();
+  const socketidToEmailMap = new Map();
+
+  io.on("connection", (socket) => {
+    console.log(`Socket Connected: ${socket.id}`);
+    
+    socket.on("room:join", (data) => {
+      const { email, room } = data;
+      emailToSocketIdMap.set(email, socket.id);
+      socketidToEmailMap.set(socket.id, email);
+      io.to(room).emit("user:joined", { email, id: socket.id });
+      socket.join(room);
+      io.to(socket.id).emit("room:join", data);
+    });
+
+    socket.on("user:call", ({ to, offer }) => {
+      io.to(to).emit("incomming:call", { from: socket.id, offer });
+    });
+
+    socket.on("call:accepted", ({ to, ans }) => {
+      io.to(to).emit("call:accepted", { from: socket.id, ans });
+    });
+
+    socket.on("peer:nego:needed", ({ to, offer }) => {
+      console.log("peer:nego:needed", offer);
+      io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+    });
+
+    socket.on("peer:nego:done", ({ to, ans }) => {
+      console.log("peer:nego:done", ans);
+      io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+    });
+    
+    socket.on("disconnect", () => {
+      const email = socketidToEmailMap.get(socket.id);
+      if (email) {
+        emailToSocketIdMap.delete(email);
+        socketidToEmailMap.delete(socket.id);
+      }
+      console.log(`Socket Disconnected: ${socket.id}`);
+    });
   });
 
-  socket.on("peer:nego:needed", ({ to, offer }) => {
-    console.log("peer:nego:needed", offer);
-    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  // Listen for incoming requests
+  server.listen(8000, () => {
+    console.log("Socket.IO server is running on port 8000");
   });
 
-  socket.on("peer:nego:done", ({ to, ans }) => {
-    console.log("peer:nego:done", ans);
-    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-  });
-});
+  // Vercel serverless function needs a response
+  res.status(200).send("Socket.IO server running");
+};
+
